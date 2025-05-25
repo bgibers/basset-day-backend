04e00e5ee77f9b767ee7aed6179a1730115ead9c
Brendan <bcgiberson@gmail.com>
2025-05-25 13:34:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express(),
 
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://host.nxt.blackbaud.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
